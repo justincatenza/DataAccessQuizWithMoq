a00739e0b7696394b4ecd520489eac167ad4c7d6
Justin Catenza <justincatenza@outlook.com>
2019-11-08 11:32:14 -0600

    updated test

--- MoqQuiz/UnitTest1.cs
+++ MoqQuiz/UnitTest1.cs
@@ -8,25 +8,31 @@ namespace MoqQuiz
     [TestClass]
     public class UnitTest1
     {
-        
-       
-        public Mock<IEmployeeDataAccess> fakeDataAccess;
-        public EmployeeBusinessLogic ebl;
-            
-        
+
+        EmployeeBusinessLogic ebl;
+        Mock<IEmployeeDataAccess> fakeDataAccess;
+
+
+
+
 
         [TestInitialize]
-        public void DoWorkTest()
+        public void SetUp()
         {
-
-            
+            fakeDataAccess = new Mock<IEmployeeDataAccess>();
             ebl = new EmployeeBusinessLogic(fakeDataAccess.Object);
+
+            fakeDataAccess.Setup(m => m.GetEmployeeSalary(It.IsAny<int>())).Equals(169069);
+            
+            
         }
 
         [TestMethod]
         public void PrintEmployeetInfo_Test_WithMoq()
         {
-            //Arrange (Handled by Initialize)                
+            //Arrange (Handled by Initialize)  
+
+            SetUp();
             //Act
             var printResult = ebl.GetEmployeeSalary(1);
             //Assert
